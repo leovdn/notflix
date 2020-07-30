8408fc0ec6011a24b237b14a10453d04cf6e9bc0
leovdn <leo_vdn@hotmail.com>
2020-07-29 22:15:27 -0300

    created form elements and rendered with useState

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-import { ExtraLink } from '../Carousel/styles';
 
 
 function Menu() {
