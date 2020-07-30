3ca52dc3d94ff9a2eaca5101e4fdf16f583b63a4
leovdn <leo_vdn@hotmail.com>
2020-07-30 16:42:13 -0300

    adjusted form style; created json file as backend

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home/';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 import cadastroVideo from './pages/cadastro/Video';
 import erro404 from './pages/erro404';
 import cadastroCategoria from './pages/cadastro/Categoria';
 
-
- 
- 
 ReactDOM.render(
   <BrowserRouter>
-  <Switch>
-    <Route path="/" component={Home} exact />
-    <Route path="/cadastro/video" component={cadastroVideo} />
-    <Route path="/cadastro/categoria" component={cadastroCategoria} />
-    <Route component={erro404} />
-
-  </Switch>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={cadastroVideo} />
+      <Route path="/cadastro/categoria" component={cadastroCategoria} />
+      <Route component={erro404} />
+    </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
