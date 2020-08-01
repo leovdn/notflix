8996ff14231a4bb846011d54599ee58dd5f140d3
leovdn <leo_vdn@hotmail.com>
2020-08-01 18:15:24 -0300

    Category Title color fixed

--- src/components/BannerMain/styles.js
+++ src/components/BannerMain/styles.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const ContentAreaContainer = styled.section`
   margin-left: 5%;
   margin-right: 5%;
-  height: 90%;
+  height: 100%;
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   justify-content: space-between;
   position: relative;
-  padding-top: 20vh;
+  padding-top: 10%;
   z-index: 10;
   @media (max-width: 800px) {
     padding-top: 100px;
@@ -17,10 +17,9 @@ export const ContentAreaContainer = styled.section`
 `;
 
 ContentAreaContainer.Item = styled.div`
-  width: 42%;
-  height: 100%;
+  width: 45%;
   display: inline-block;
-  justify-self: self-end;
+  margin-bottom: 50px;
   @media (max-width: 800px) {
     width: 100%;
   }
@@ -37,9 +36,8 @@ ContentAreaContainer.Category = styled.h1`
   text-align: center;
   display: inline-block; 
   padding: 25px;
-  line-height: 1.2;
+  line-height: 1;
   border-radius: 4px;
-
   @media (max-width: 800px) {
     display: none;
     font-size: 18px;
@@ -55,12 +53,11 @@ ContentAreaContainer.Description = styled.p`
 
 ContentAreaContainer.Title = styled.h2`
   font-style: normal;
-  font-weight: bold;
+  font-weight: 300;
   font-size: 40px;
   line-height: 1;
   margin-top: 0;
   margin-bottom: 32px;
-
   @media (max-width: 800px) {
     font-size: 32px;
     text-align: center;
@@ -68,9 +65,10 @@ ContentAreaContainer.Title = styled.h2`
 `;
 
 export const BannerMainContainer = styled.section`
-  height: 80vh;
+  height: 100vh;
   position: relative;
   color: #fff;
+  background-image: 
   background-image: ${({ backgroundImage }) => `url(${backgroundImage})`}; 
   background-size: cover;
   background-position: center;
@@ -78,7 +76,6 @@ export const BannerMainContainer = styled.section`
     height: auto;
     min-height: 50vh;
   }
-
   &:after,
   &:before {
     content: "";
@@ -89,21 +86,19 @@ export const BannerMainContainer = styled.section`
     margin: auto;
     height: 20%;
   }
-
   &:before {
     top: 0;
     height: 100%;
-    background: rgba(0,0,0,0.4);
+    background: rgba(0,0,0,0.5);
   }
-
   &:after {
     bottom: 0;
-    background: linear-gradient(0deg, #141414 0%, transparent 50%);
+    background: linear-gradient(0deg, #141414 0%, transparent 100%);
   }
 `;
 
 export const WatchButton = styled.button`
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Georgia', sans-serif;
   box-sizing: border-box;
   cursor: pointer;
   padding: 16px 24px;
